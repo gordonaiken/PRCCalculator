557a1cacc56da5261de0782fe65d458c3fe7b218
G <aiken.gordon@gmail.com>
2020-11-30 13:59:31 +0000

    Initial commit of calculator.py and readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # PRCCalculator
 Clone the repo and complete the calculator
+
+This is an example project to experiment with cloning github repos for the class MCOMD0PRC: Programming Concepts.
+
+Please clone the repo and complete the calculator code.
---
+++ calculator.py
@@ -0,0 +1,22 @@
+"""
+Basic Calculator for two numbers which can perform the following operations:
+addition, subtraction, multiplication and division
+"""
+
+def addition(num1, num2):
+
+    return num1 + num2
+
+def subtract(num1, num2):
+
+    return num1 - num2
+
+def multiply(num1, num2):
+    pass
+
+def divide(num1, num2):
+    pass
+
+#todo: implement exponent
+
+#todo: implement user input
